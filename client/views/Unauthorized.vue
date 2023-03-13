f8079bee73a06e143820d2f2f0fd6081dbf85add
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-03-13 12:43:40 +0000

    Updated Unauthorized.vue

--- client/views/Unauthorized.vue
+++ client/views/Unauthorized.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="NotFoundDiv">
         <RouterLink :to="{name: 'home'}">
-            <img src="/Images/sail.png" alt="Image generated using Midjourney AI.">
+            <img src="/images/sail.png" alt="Image generated using Midjourney AI.">
         </RouterLink>
         <h3>⛺️ <br> 401 Unauthorized access</h3>
         <p>Only the admin can access this page. Please return to the <RouterLink class="color bold" :to="{name: 'home'}">Homepage</RouterLink>!</p>
