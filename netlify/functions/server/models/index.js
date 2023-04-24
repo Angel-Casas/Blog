275581f86dc2c18ed909f91ceab3246c69541f61
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-04-24 15:48:56 +0100

    Re

--- netlify/functions/server/models/index.js
+++ netlify/functions/server/models/index.js
@@ -10,6 +10,9 @@ const db = {};
 
 db.mongoose = mongoose;
 db.url = dbConfig.url;
+db.options = dbConfig.mongoOptions;
 db.posts = require('./post.model.js')(mongoose);
+db.tags = require('./tag.model.js')(mongoose);
+db.users = require('./user.model.js')(mongoose);
 
 module.exports = db;
